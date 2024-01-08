792073c39831b3e486e9914b3e69e2511c091846
Adricf28 <adricf2807@gmail.com>
2024-01-08 10:47:28 +0100

    08012024 1 Noupueopensa

---
+++ LibroEjerciciosJava/Herencia/Cuenta/Cuenta.java
@@ -0,0 +1,34 @@
+public class Cuenta {
+    //ATRIBUTOS
+    protected float saldo;
+    protected int consignaciones=0;
+    protected int retiros=0;
+    protected float tasaAnual;
+    protected float comisionMensual=0;
+
+    //CONSTRUCTORES
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+
+    //METODOS
+    public void consignar(float nuevoSaldo) {
+        this.saldo += nuevoSaldo;
+    }
+    public void retirar(float saldo) {
+        if (saldo <= this.saldo) {
+            this.saldo -= saldo;
+        }
+    }
+    public float interes(){
+        float tasaMensual = this.tasaAnual % 12;
+        return this.saldo += (this.saldo * tasaMensual);
+    }
+    public void extracto() {
+        //nosebro 2
+    }
+    public void imprimir() {
+        System.out.printf("Saldo: %f\nConsignaciones: %d\nRetiros: %d\nTasa Anual: %f\nComision Mensual: %f", this.saldo, this.consignaciones, this.retiros, this.tasaAnual, this.comisionMensual);
+    }
+}
---
+++ LibroEjerciciosJava/Herencia/Cuenta/CuentaAhorros.java
@@ -0,0 +1,3 @@
+public class CuentaAhorros {
+    protected boolean estado;
+}
